e26b53a594b2334de66c7a7735fb049020e89ae5
utkristshrestha <utkrist.shrestha12@gmail.com>
2022-08-13 23:36:00 +1000

    Updates

---
+++ precache-manifest.4c014e810870f8643455da48e50d6bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27eef189212f69526a4a",
+    "url": "/UTool/static/css/main.0a9ecdb5.chunk.css"
+  },
+  {
+    "revision": "27eef189212f69526a4a",
+    "url": "/UTool/static/js/main.27eef189.chunk.js"
+  },
+  {
+    "revision": "5fa167853b60ce28b276",
+    "url": "/UTool/static/js/1.5fa16785.chunk.js"
+  },
+  {
+    "revision": "e156963c05aab9cf239c",
+    "url": "/UTool/static/js/runtime~main.e156963c.js"
+  },
+  {
+    "revision": "d25f4b28e7eef12c87c47318b70dd04f",
+    "url": "/UTool/index.html"
+  }
+];
